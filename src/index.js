840a19a621d90a42314df935d494d6642e8a4d91
Ian Ndungu <ianndungu06@gmail.com>
2023-06-21 12:58:44 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById("create-task-form");
+  form.addEventListener("submit" , draw);
+  function draw (e){
+    e.preventDefault()
+    record(e.target[0].value)
+    form.reset()
+
+  }
 });
+
+function record(text){
+  var display = document.createElement("li")
+  var store = document.createElement("button")
+  store.addEventListener("click",Delete)
+  store.textContent = "delete"
+  store.style.marginLeft = "10px"
+  display.append(text)
+  display.appendChild(store)
+  const task = document.getElementById("tasks")
+  task.appendChild(display)
+}
+function Delete(e){
+  e.target.parentNode.remove()
+}
